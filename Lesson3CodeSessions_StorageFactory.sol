837f1fd8f0ff1c4198b53aae5dcd1afbb785899e
Joshua Gorospe <joshua.gorospe@gmail.com>
2024-04-03 04:54:19 +0000

    Add current status

---
+++ Lesson3CodeSessions_StorageFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.7;
+
+import "./SimpleStorage.sol"; 
+
+contract StorageFactory {
+
+// Examples in this code focuses on importing code and code inheritance
+    
+    SimpleStorage[] public simpleStorageArray;
+    
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+    
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI 
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+}
