d042a2da51a15f4cf64fe742ca0d2eb468c61f4c
Joshua Gorospe <joshua.gorospe@gmail.com>
2024-04-03 05:00:57 +0000

    Add current status

--- Lesson3CodeSessions_StorageFactory.sol
+++ Lesson3CodeSessions_StorageFactory.sol
@@ -4,6 +4,36 @@ pragma solidity ^0.8.7;
 
 import "./SimpleStorage.sol"; 
 
+/*
+contract SimpleStorage {
+
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    People[] public people;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
+    
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
+*/
+// It is possible to have 2 contracts in 1 Solidity file, it is better to use import instead
+
 contract StorageFactory {
 
 // Examples in this code focuses on importing code and code inheritance, and also demonstrates how contracts can deploy other contracts (through import)
